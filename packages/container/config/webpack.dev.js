bb72c7d9ae0114749c4e1fe4b78970e25a6d4adf
Sebastian Bejas <jbejas@gmail.com>
2023-12-04 16:16:41 -0500

    Update index.html bug

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,33 +2,14 @@ const { merge } = require('webpack-merge')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
-const path = require('path')
 
 const devConfig = {
   mode: 'development',
   devServer: {
     port: 8080,
     historyApiFallback: {
-      index: 'index.html',
+      index: '/index.html',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
-      }
-    ]
   },
   plugins: [
     new ModuleFederationPlugin({
